47d3101693fb1f11ec0621244c1a22b5794528ea
yousseflj9 <youssefmomen@outlook.com>
2025-11-06 12:08:41 +0100

    test1

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import ProfilePage from './components/UserProfile';
-import {UserProvider} from './UserContext';
+import {UserProvider} from './context/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
