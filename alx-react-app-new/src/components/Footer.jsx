9c13e21be0408c13b358cc3171985ff90f5d517a
yousseflj9 <youssefmomen@outlook.com>
2025-11-06 11:03:39 +0100

    feat: initialize React app with Vite and basic components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+    return (
+<footer style={{border:"3px solid gray", position:'absolute'}}>
+  <p>© 2023 City Lovers</p>
+</footer>
+
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+
+function MainContent() {
+    return (
+   <main style={{border:"3px solid gray", position:""}}>
+  <p style={{padding: '10px', margin: '10px', textAlign:'center'  }}>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    );
+}
+
+export default MainContent;
