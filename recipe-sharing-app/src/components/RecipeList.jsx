3c75645e59b55a3da1a02ec59e595d54e54cbb25
yousseflj9 <youssefmomen@outlook.com>
2025-11-27 10:16:32 +0100

    fix: update import paths for recipe components to use local RecipeStore

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
  // RecipeList component
- import { useRecipeStore } from '../store/useRecipeStore';
+ import { useRecipeStore } from './RecipeStore';
 
  const RecipeList = () => {
    const recipes = useRecipeStore(state => state.recipes);
