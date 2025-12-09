08d285628b2eaeef9531c2a0689a12a1b917d261
yousseflj9 <youssefmomen@outlook.com>
2025-12-09 18:38:04 +0100

    lst check

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-
+// the query parameter should be constructed in the calling function already have all necessary filters applied : username, location, minRepos
 export default async function fetchUserData(query) {
     const response = await axios.get(`https://api.github.com/search/users?q=${query} `, {
     });
