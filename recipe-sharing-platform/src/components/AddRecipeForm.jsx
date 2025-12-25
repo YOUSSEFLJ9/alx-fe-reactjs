8dc5dd3811f9dda964ab7f995acc56e8c26b6111
yousseflj9 <youssefmomen@outlook.com>
2025-12-25 10:21:13 +0100

    feat: add validation and error handling to AddRecipeForm

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -9,6 +9,8 @@ export default function AddRecipeForm ()
     const [image, setImage] = useState("");
     const [ingredients, setIngredients] = useState([""]);
     const [instructions, setInstructions] = useState([""]);
+    const [errors, setErrors] = useState("");
+    const [validate, setValidate] = useState(false);
     
     const handleSubmit = (e) =>
     {
@@ -21,13 +23,19 @@ export default function AddRecipeForm ()
             ingredients: ingredients.filter(ing => ing.trim() !== ""),
             instructions: instructions.filter(inst => inst.trim() !== "") //steps
         };
+        if (!newRecipe.id || !newRecipe.title || !newRecipe.summary || !newRecipe.image || newRecipe.ingredients.length === 0 || newRecipe.instructions.length === 0) {
+            setErrors("All fields are required.");
+            setValidate(false);
+            return;
+        }
+        else {
+            setErrors("");
+            setValidate(true);
+        }
         console.log("New Recipe Submitted: ", newRecipe);
     }
     return (
         <div className="min-h-screen bg-blue-50 p-8">
-            <h1 className="text-4xl font-bold text-red-600 underline mb-8">
-                Add New Recipe
-            </h1>
             <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg shadow-md max-w-2xl mx-auto">
                 <div className="mb-4">
                     <label className="block text-gray-700 mb-2">Title</label>
@@ -65,6 +73,8 @@ export default function AddRecipeForm ()
                 </div>
                 <button type="submit" className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">Submit Recipe</button>
             </form>
+            {errors && <p className="text-red-500 mt-4 text-center text-xl">{errors}</p>}
+            {validate && <p className=" text-green-500 mt-4 text-center text-xl">Recipe submitted successfully!</p>}
         </div>
     );
     
